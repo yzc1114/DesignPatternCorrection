6b4228333b6c2def99b8800830d3667ae6b26901
ZhentingYan <632950864@qq.com>
2019-11-07 18:22:43 +0800

    TemplateMethod

--- src/Scene_InPark/Test/TemplateMethodPatternTest.java
+++ src/Scene_InPark/Test/TemplateMethodPatternTest.java
@@ -19,8 +19,8 @@ public class TemplateMethodPatternTest {
         //默认设置下午计划
         mPlan.setStrategy(1);
 
-        System.out.println("游园规划：进入迪士尼 上午活动（待计划） 吃午饭 过山车 游乐园 滑雪 出门离去 吃晚饭 去住宿");
-        System.out.println("请自行规划上午计划：");
+        System.out.println("这是迪士尼乐园为您精心推荐的游园规划：进入迪士尼 上午活动（由您决定） 吃午饭 过山车 游乐园 滑雪 出门离去 吃晚饭 去住宿");
+        System.out.println("迪士尼有很多神秘的目的地可以探索，您上午的行程需要自己决定哦～：");
         if(scan.hasNextLine()) {
             morningPlan = scan.nextLine();
         }
