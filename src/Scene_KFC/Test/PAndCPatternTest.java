791e3def4c2b2a199155c0ff04e940370da32b5d
Zhou <playerzhou@icloud.com>
2019-11-13 18:28:16 +0800

    update

--- src/Scene_KFC/Test/PAndCPatternTest.java
+++ src/Scene_KFC/Test/PAndCPatternTest.java
@@ -9,31 +9,28 @@ public class PAndCPatternTest {
         System.out.println("\n"+"测试一：");
 
         Storage storage = new Storage();
-        Producer p1 = new Producer(storage);
-        Producer p2 = new Producer(storage);
-        Producer p3 = new Producer(storage);
-        Producer p4 = new Producer(storage);
-        Producer p5 = new Producer(storage);
+        Producer p1=new Producer(storage);
+        Producer p2=new Producer(storage);
+        Consumer c1=new Consumer(storage);
+        Consumer c2=new Consumer(storage);
 
-        Consumer c1 = new Consumer(storage);
-        Consumer c2 = new Consumer(storage);
-        Consumer c3 = new Consumer(storage);
-        p1.setNum(10);
+        c1.setNum(10);
+        p1.setNum(20);
+        c2.setNum(30);
         p2.setNum(20);
-        p3.setNum(10);
-        p4.setNum(80);
-        p5.setNum(10);
-        c1.setNum(50);
-        c2.setNum(20);
-        c3.setNum(20);
         c1.start();
         c2.start();
-        c3.start();
         p1.start();
         p2.start();
-        p3.start();
-        p4.start();
-        p5.start();
+        try{
+            c1.join();
+            c2.join();
+            p1.join();
+            p2.join();
+        }catch (Exception e){
+
+        }
+
 
         System.out.println("\n" +"----------------------------------------");
     }
